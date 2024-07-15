bf89f607af44aa0aa1dd049983e205e3521d61cf
Ilshat Osmanov <28092001i@gmail.com>
2024-07-15 12:54:26 +0500

    Updates

--- assets/CCAPIU72.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as r,a5 as a,t as n}from"./R5l8HrxH.js";const o={};function s(e,c){return n(),r("div",null,[a(e.$slots,"default")])}const _=t(o,[["render",s]]);export{_ as default};
---
+++ assets/CfhJ4hzW.js
@@ -0,0 +1,1 @@
+import{T as r,r as a,j as t}from"./C36Jqkuh.js";const u=r("loader",()=>{const e=a(!1);return{getLoader:t(()=>e.value),setLoader:o=>{e.value=o}}});export{u};
